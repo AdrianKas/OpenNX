984dea9d125138e83c532fddb49b93a376920188
AdrianKas <66158096+AdrianKas@users.noreply.github.com>
2023-09-19 13:58:56 +0200

    First Commit

---
+++ my_first_journal.py
@@ -0,0 +1,337 @@
+﻿# NX 2206
+# Journal created by adriaka on Tue Sep 19 13:45:44 2023 W. Europe Daylight Time
+#
+import math
+import NXOpen
+import NXOpen.Features
+import NXOpen.GeometricUtilities
+import NXOpen.Preferences
+def main() : 
+
+    theSession  = NXOpen.Session.GetSession()
+    workPart = theSession.Parts.Work
+    displayPart = theSession.Parts.Display
+    # ----------------------------------------------
+    #   Menu: Insert->Sketch
+    # ----------------------------------------------
+    markId1 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Enter Sketch")
+    
+    markId2 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Update Model from Sketch")
+    
+    theSession.BeginTaskEnvironment()
+    
+    markId3 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Start")
+    
+    sketchInPlaceBuilder1 = workPart.Sketches.CreateSketchInPlaceBuilder2(NXOpen.Sketch.Null)
+    
+    origin1 = NXOpen.Point3d(0.0, 0.0, 0.0)
+    normal1 = NXOpen.Vector3d(0.0, 0.0, 1.0)
+    plane1 = workPart.Planes.CreatePlane(origin1, normal1, NXOpen.SmartObject.UpdateOption.WithinModeling)
+    
+    sketchInPlaceBuilder1.PlaneReference = plane1
+    
+    unit1 = workPart.UnitCollection.FindObject("MilliMeter")
+    expression1 = workPart.Expressions.CreateSystemExpressionWithUnits("0", unit1)
+    
+    expression2 = workPart.Expressions.CreateSystemExpressionWithUnits("0", unit1)
+    
+    sketchAlongPathBuilder1 = workPart.Sketches.CreateSketchAlongPathBuilder(NXOpen.Sketch.Null)
+    
+    simpleSketchInPlaceBuilder1 = workPart.Sketches.CreateSimpleSketchInPlaceBuilder()
+    
+    theSession.SetUndoMarkName(markId3, "Create Sketch Dialog")
+    
+    simpleSketchInPlaceBuilder1.UseWorkPartOrigin = False
+    
+    datumAxis1 = workPart.Datums.FindObject("DATUM_CSYS(0) X axis")
+    direction1 = workPart.Directions.CreateDirection(datumAxis1, NXOpen.Sense.Forward, NXOpen.SmartObject.UpdateOption.WithinModeling)
+    
+    datumPlane1 = workPart.Datums.FindObject("DATUM_CSYS(0) XY plane")
+    datumCsys1 = workPart.Features.FindObject("DATUM_CSYS(0)")
+    point1 = datumCsys1.FindObject("POINT 1")
+    xform1 = workPart.Xforms.CreateXformByPlaneXDirPoint(datumPlane1, direction1, point1, NXOpen.SmartObject.UpdateOption.WithinModeling, 0.625, False, False)
+    
+    cartesianCoordinateSystem1 = workPart.CoordinateSystems.CreateCoordinateSystem(xform1, NXOpen.SmartObject.UpdateOption.WithinModeling)
+    
+    simpleSketchInPlaceBuilder1.CoordinateSystem = cartesianCoordinateSystem1
+    
+    simpleSketchInPlaceBuilder1.HorizontalReference.Value = datumAxis1
+    
+    point2 = simpleSketchInPlaceBuilder1.SketchOrigin
+    
+    simpleSketchInPlaceBuilder1.SketchOrigin = point2
+    
+    point3 = workPart.Points.CreatePoint(point1, NXOpen.Xform.Null, NXOpen.SmartObject.UpdateOption.WithinModeling)
+    
+    markId4 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Create Sketch")
+    
+    theSession.DeleteUndoMark(markId4, None)
+    
+    markId5 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Create Sketch")
+    
+    theSession.Preferences.Sketch.CreateInferredConstraints = False
+    
+    theSession.Preferences.Sketch.ContinuousAutoDimensioning = False
+    
+    theSession.Preferences.Sketch.DimensionLabel = NXOpen.Preferences.SketchPreferences.DimensionLabelType.Expression
+    
+    theSession.Preferences.Sketch.TextSizeFixed = False
+    
+    theSession.Preferences.Sketch.FixedTextSize = 3.0
+    
+    theSession.Preferences.Sketch.DisplayParenthesesOnReferenceDimensions = True
+    
+    theSession.Preferences.Sketch.DisplayReferenceGeometry = False
+    
+    theSession.Preferences.Sketch.DisplayShadedRegions = True
+    
+    theSession.Preferences.Sketch.FindMovableObjects = True
+    
+    theSession.Preferences.Sketch.ConstraintSymbolSize = 3.0
+    
+    theSession.Preferences.Sketch.DisplayObjectColor = False
+    
+    theSession.Preferences.Sketch.DisplayObjectName = True
+    
+    theSession.Preferences.Sketch.EditDimensionOnCreation = True
+    
+    theSession.Preferences.Sketch.CreateDimensionForTypedValues = True
+    
+    nXObject1 = simpleSketchInPlaceBuilder1.Commit()
+    
+    sketch1 = nXObject1
+    feature1 = sketch1.Feature
+    
+    markId6 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "update")
+    
+    nErrs1 = theSession.UpdateManager.DoUpdate(markId6)
+    
+    sketch1.Activate(NXOpen.Sketch.ViewReorient.TrueValue)
+    
+    theSession.Preferences.Sketch.FindMovableObjects = True
+    
+    sketchFindMovableObjectsBuilder1 = workPart.Sketches.CreateFindMovableObjectsBuilder()
+    
+    nXObject2 = sketchFindMovableObjectsBuilder1.Commit()
+    
+    sketchFindMovableObjectsBuilder1.Destroy()
+    
+    theSession.DeleteUndoMark(markId5, None)
+    
+    theSession.SetUndoMarkName(markId3, "Create Sketch")
+    
+    sketchInPlaceBuilder1.Destroy()
+    
+    sketchAlongPathBuilder1.Destroy()
+    
+    simpleSketchInPlaceBuilder1.Destroy()
+    
+    workPart.Points.DeletePoint(point3)
+    
+    try:
+        # Expression is still in use.
+        workPart.Expressions.Delete(expression2)
+    except NXOpen.NXException as ex:
+        ex.AssertErrorCode(1050029)
+        
+    try:
+        # Expression is still in use.
+        workPart.Expressions.Delete(expression1)
+    except NXOpen.NXException as ex:
+        ex.AssertErrorCode(1050029)
+        
+    plane1.DestroyPlane()
+    
+    theSession.DeleteUndoMarksUpToMark(markId2, None, True)
+    
+    markId7 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Open Sketch")
+    
+    theSession.ActiveSketch.SetName("SKETCH_000")
+    
+    # ----------------------------------------------
+    #   Menu: Insert->Curve->Circle...
+    # ----------------------------------------------
+    markId8 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Profile short list")
+    
+    markId9 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Curve")
+    
+    theSession.SetUndoMarkVisibility(markId9, "Curve", NXOpen.Session.MarkVisibility.Visible)
+    
+    nXMatrix1 = theSession.ActiveSketch.Orientation
+    
+    center1 = NXOpen.Point3d(0.0, 0.0, 0.0)
+    arc1 = workPart.Curves.CreateArc(center1, nXMatrix1, 191.84745977326352, 0.0, ( 360.0 * math.pi/180.0 ))
+    
+    theSession.ActiveSketch.AddGeometry(arc1, NXOpen.Sketch.InferConstraintsOption.InferNoConstraints)
+    
+    theSession.ActiveSketch.Update()
+    
+    # ----------------------------------------------
+    #   Dialog Begin Circle
+    # ----------------------------------------------
+    # ----------------------------------------------
+    #   Menu: Task->Finish Sketch
+    # ----------------------------------------------
+    sketchFindMovableObjectsBuilder2 = workPart.Sketches.CreateFindMovableObjectsBuilder()
+    
+    nXObject3 = sketchFindMovableObjectsBuilder2.Commit()
+    
+    sketchFindMovableObjectsBuilder2.Destroy()
+    
+    sketchWorkRegionBuilder1 = workPart.Sketches.CreateWorkRegionBuilder()
+    
+    sketchWorkRegionBuilder1.Scope = NXOpen.SketchWorkRegionBuilder.ScopeType.EntireSketch
+    
+    nXObject4 = sketchWorkRegionBuilder1.Commit()
+    
+    sketchWorkRegionBuilder1.Destroy()
+    
+    theSession.ActiveSketch.CalculateStatus()
+    
+    theSession.Preferences.Sketch.SectionView = False
+    
+    markId10 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Deactivate Sketch")
+    
+    theSession.ActiveSketch.Deactivate(NXOpen.Sketch.ViewReorient.TrueValue, NXOpen.Sketch.UpdateLevel.Model)
+    
+    theSession.DeleteUndoMarksSetInTaskEnvironment()
+    
+    theSession.EndTaskEnvironment()
+    
+    # ----------------------------------------------
+    #   Menu: Insert->Design Feature->Extrude...
+    # ----------------------------------------------
+    markId11 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Start")
+    
+    extrudeBuilder1 = workPart.Features.CreateExtrudeBuilder(NXOpen.Features.Feature.Null)
+    
+    section1 = workPart.Sections.CreateSection(0.0094999999999999998, 0.01, 0.5)
+    
+    extrudeBuilder1.Section = section1
+    
+    extrudeBuilder1.AllowSelfIntersectingSection(True)
+    
+    unit2 = extrudeBuilder1.Draft.FrontDraftAngle.Units
+    
+    expression3 = workPart.Expressions.CreateSystemExpressionWithUnits("2.00", unit2)
+    
+    extrudeBuilder1.DistanceTolerance = 0.01
+    
+    extrudeBuilder1.BooleanOperation.Type = NXOpen.GeometricUtilities.BooleanOperation.BooleanType.Create
+    
+    targetBodies1 = [NXOpen.Body.Null] * 1 
+    targetBodies1[0] = NXOpen.Body.Null
+    extrudeBuilder1.BooleanOperation.SetTargetBodies(targetBodies1)
+    
+    extrudeBuilder1.Limits.StartExtend.Value.SetFormula("0")
+    
+    extrudeBuilder1.Limits.EndExtend.Value.SetFormula("25")
+    
+    extrudeBuilder1.Offset.StartOffset.SetFormula("0")
+    
+    extrudeBuilder1.Offset.EndOffset.SetFormula("5")
+    
+    smartVolumeProfileBuilder1 = extrudeBuilder1.SmartVolumeProfile
+    
+    smartVolumeProfileBuilder1.OpenProfileSmartVolumeOption = False
+    
+    smartVolumeProfileBuilder1.CloseProfileRule = NXOpen.GeometricUtilities.SmartVolumeProfileBuilder.CloseProfileRuleType.Fci
+    
+    theSession.SetUndoMarkName(markId11, "Extrude Dialog")
+    
+    section1.DistanceTolerance = 0.01
+    
+    section1.ChainingTolerance = 0.0094999999999999998
+    
+    section1.SetAllowedEntityTypes(NXOpen.Section.AllowTypes.OnlyCurves)
+    
+    markId12 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, None)
+    
+    selectionIntentRuleOptions1 = workPart.ScRuleFactory.CreateRuleOptions()
+    
+    selectionIntentRuleOptions1.SetSelectedFromInactive(False)
+    
+    curves1 = [NXOpen.ICurve.Null] * 1 
+    curves1[0] = arc1
+    seedPoint1 = NXOpen.Point3d(-25.266642306352725, -12.589929710837916, 0.0)
+    regionBoundaryRule1 = workPart.ScRuleFactory.CreateRuleRegionBoundary(sketch1, curves1, seedPoint1, 0.01, selectionIntentRuleOptions1)
+    
+    selectionIntentRuleOptions1.Dispose()
+    section1.AllowSelfIntersection(True)
+    
+    section1.AllowDegenerateCurves(False)
+    
+    rules1 = [None] * 1 
+    rules1[0] = regionBoundaryRule1
+    helpPoint1 = NXOpen.Point3d(0.0, 0.0, 0.0)
+    section1.AddToSection(rules1, NXOpen.NXObject.Null, NXOpen.NXObject.Null, NXOpen.NXObject.Null, helpPoint1, NXOpen.Section.Mode.Create, False)
+    
+    theSession.DeleteUndoMark(markId12, None)
+    
+    markId13 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "section mark")
+    
+    markId14 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, None)
+    
+    theSession.DeleteUndoMark(markId14, None)
+    
+    direction2 = workPart.Directions.CreateDirection(sketch1, NXOpen.Sense.Forward, NXOpen.SmartObject.UpdateOption.WithinModeling)
+    
+    extrudeBuilder1.Direction = direction2
+    
+    extrudeBuilder1.BooleanOperation.Type = NXOpen.GeometricUtilities.BooleanOperation.BooleanType.Create
+    
+    targetBodies2 = [NXOpen.Body.Null] * 1 
+    targetBodies2[0] = NXOpen.Body.Null
+    extrudeBuilder1.BooleanOperation.SetTargetBodies(targetBodies2)
+    
+    targetBodies3 = []
+    extrudeBuilder1.BooleanOperation.SetTargetBodies(targetBodies3)
+    
+    theSession.DeleteUndoMark(markId13, None)
+    
+    extrudeBuilder1.BooleanOperation.Type = NXOpen.GeometricUtilities.BooleanOperation.BooleanType.Create
+    
+    targetBodies4 = [NXOpen.Body.Null] * 1 
+    targetBodies4[0] = NXOpen.Body.Null
+    extrudeBuilder1.BooleanOperation.SetTargetBodies(targetBodies4)
+    
+    targetBodies5 = []
+    extrudeBuilder1.BooleanOperation.SetTargetBodies(targetBodies5)
+    
+    extrudeBuilder1.Limits.EndExtend.Value.SetFormula("120")
+    
+    extrudeBuilder1.BooleanOperation.Type = NXOpen.GeometricUtilities.BooleanOperation.BooleanType.Create
+    
+    targetBodies6 = [NXOpen.Body.Null] * 1 
+    targetBodies6[0] = NXOpen.Body.Null
+    extrudeBuilder1.BooleanOperation.SetTargetBodies(targetBodies6)
+    
+    targetBodies7 = []
+    extrudeBuilder1.BooleanOperation.SetTargetBodies(targetBodies7)
+    
+    markId15 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Extrude")
+    
+    theSession.DeleteUndoMark(markId15, None)
+    
+    markId16 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Extrude")
+    
+    extrudeBuilder1.ParentFeatureInternal = False
+    
+    feature2 = extrudeBuilder1.CommitFeature()
+    
+    theSession.DeleteUndoMark(markId16, None)
+    
+    theSession.SetUndoMarkName(markId11, "Extrude")
+    
+    expression4 = extrudeBuilder1.Limits.StartExtend.Value
+    expression5 = extrudeBuilder1.Limits.EndExtend.Value
+    extrudeBuilder1.Destroy()
+    
+    workPart.Expressions.Delete(expression3)
+    
+    # ----------------------------------------------
+    #   Menu: Tools->Journal->Stop Recording
+    # ----------------------------------------------
+    
+if __name__ == '__main__':
+    main()
